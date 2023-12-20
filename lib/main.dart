259b42739764720b72cba793ce054a6afa79b34a
abbosking <abbosyuldashev640@gmail.com>
2023-12-20 15:15:06 +0500

    test main

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Algorithms Store',
+      title: 'Algorithms Stordbydyubsde',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
